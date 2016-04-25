81490613ceb2a58717dee0bfd0c7a94f4955a4f6
Ali Rashed <alitarekrashed@gmail.com>
2016-04-25 00:09:11 -0500

    commented the commands

--- src/main/scala/Piet.scala
+++ src/main/scala/Piet.scala
@@ -1,47 +1,79 @@
 package pietcs345
 import scala.collection.mutable
 
-/**Important Note: Methods are implemented based on the mutable stack
-	as the underlying container. This is probably poor design, should
-	use the ProgramStack methods wherever possible - in case we decide
-	to change the container. Not sure how to do it in Scala yet. **/
+/*
+ *	Functions as the stack used in evaluating a piet program.
+ *	Various commands can be called on the stack, based on how the
+ *	piet image is interpreted. 
+ *
+ *	stack: internal container (a mutable Stack[Int]), that is used as 
+ *		the underlying container of the ProgramStack  
+ */
 class ProgramStack{
 	var stack = new mutable.Stack[Int]()
 
-	def push(value: Int) = {
-		stack = stack.push(value)
+	/* 
+	 *	Pushes a value to the stack
+	 */
+	def push(x: Int) = {
+		stack = stack.push(x)
 	}
 
+	/*
+	 * Pops the top value off the stack
+	 */ 
 	def pop() : Int = {
 		val item = stack.pop()
 		stack = stack
 		return item
 	}
 
+	/*
+	 * Pops the top two values off the stack, adds them,
+	 * and then pushes the result back onto the stack
+	 */
 	def add() = {
 		val first = stack.pop()
 		val second = stack.pop()
 		stack = stack.push(second + first)
 	}
 
+	/*
+	 * Pops the top two values off the stack, subtracts the
+	 * first value from the second value and then pushes the
+	 * result back onto the stack
+	 */
 	def subtract() = {
 		val first = stack.pop()
 		val second = stack.pop()
 		stack = stack.push(second - first)
 	}
 
+	/*
+	 * Pops the top two values off the stack, multiplies them,
+	 * and then pushes the result back onto the stack
+	 */
 	def multiply() = {
 		val first = stack.pop()
 		val second = stack.pop()
 		stack = stack.push(second * first)
 	}
 
+	/*
+	 * Pops the top two values off the stack, divides the second value
+	 * by the first value, and then puhses the result back onto the stack
+	 */
 	def divide() = {
 		val first = stack.pop()
 		val second = stack.pop()
 		stack = stack.push(second / first)
 	}
 
+	/*
+	 * Pops the top two values off the stack, calculates the second value
+	 * modulo the first value, and pushes the result back on the stack.
+	 * Note: return the 'least positive residue'
+	 */
 	def mod() = {
 		val first = stack.pop()
 		val second = stack.pop()
@@ -52,6 +84,10 @@ class ProgramStack{
 		stack = stack.push(mod)
 	}
 
+	/*
+	 * If the top value of the stack is non-zero, replace it zero,
+	 * otherwise replace it with 1.
+	 */
 	def not() = {
 		val top = stack.pop()
 		if(top == 0){
@@ -61,6 +97,10 @@ class ProgramStack{
 		}
 	}
 
+	/*
+	 * Pops the top two values off the stock. If the second value is greater
+	 * than the first value, push 1 onto the stack, otherwise push 0.
+	 */
 	def greater() = {
 		val first = stack.pop()
 		val second = stack.pop()
@@ -71,11 +111,20 @@ class ProgramStack{
 		}
 	}
 
+	/*
+	 * Pushes a copy of the top value onto the stack.
+	 */
 	def duplicate() = {
 		val top = stack.top
 		stack = stack.push(top)
 	}
 
+	/*
+	 * Pops the top two values off the stack, and rolls the stack to a depth
+	 * equal to the second value, by a number of rolls equal to the first value.
+	 * 
+	 * If the first value is negative, roll in the opposite direction
+	 */
 	def roll() = {
 		var rolls = stack.pop()
 		val depth = stack.pop()
@@ -100,10 +149,16 @@ class ProgramStack{
 		}
 	}
 
+	/*
+	 * Returns the top value on the stack
+	 */
 	def top() : Int = {
 		return stack.top
 	}
 
+	/*
+	 * Returns the number of elements in the stack
+	 */
 	def length() : Int = {
 		return stack.length
 	}
